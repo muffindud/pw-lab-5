13004571407b9d83011f379de95e7e8fd8a468dc
muffindud <ccatlabuga2002@gmail.com>
2024-03-22 12:29:27 +0200

    add -h output

--- go2web.py
+++ go2web.py
@@ -11,7 +11,9 @@ DATA_PER_REQUEST = 1024
 
 
 def print_help():
-    ...
+    print("go2web -u <URL>         # make an HTTP request to the specified URL and print the response")
+    print("go2web -s <search-term> # make an HTTP request to search the term using your favorite search engine and print top 10 results")
+    print("go2web -h               # show this help")
 
 
 def make_http_request(url: str) -> dict:
